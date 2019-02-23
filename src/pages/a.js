83782a4c0e473a3dc5efd51752877425198e93da
yq <Leafqun@yeah.net>
2019-02-23 17:56:40 +0800

    1

--- src/pages/a.js
+++ src/pages/a.js
@@ -5,7 +5,7 @@ const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
 export default function() {
   return (
     <div>
-      <Button type="primary">测试</Button>
+      <Button type="primary">测试121</Button>
       <Countdown title="Million Seconds" value={deadline} format="HH:mm:ss:SSS" />
     </div>
   )
