d57ce20f00fe1071083bdcb5a5b12603d81d1313
yq <Leafqun@yeah.net>
2019-02-23 21:26:33 +0800

    deploy5

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cnpm install
 
 cp $PROJ_PATH/blog-manage-umi/dist/umi.js $DEPLOY_PATH/
 cp $PROJ_PATH/blog-manage-umi/dist/umi.css $DEPLOY_PATH/
-cp $PROJ_PATH/blog-manage-umi/dist/index.html $DEPLOY_PATH/blog-manage-umi
+cp $PROJ_PATH/blog-manage-umi/dist/index.html $DEPLOY_PATH/blog-manage-umi/
