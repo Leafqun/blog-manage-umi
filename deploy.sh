5fe4d5c3c25441c2ba42e7e18bfe58ea1102a27a
yq <Leafqun@yeah.net>
2019-02-23 18:36:45 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+
+cd $PROJ_PATH/blog-mange-umi
+cnpm install
+
+cp $PROJ_PATH/blog-mange-umi/dist/umi.js $DEPLOY/
+cp $PROJ_PATH/blog-mange-umi/dist/umi.css $DEPLOY/
+cp $PROJ_PATH/blog-mange-umi/dist/index.html $DEPLOY/blog-mange-umi
